111ddf50d9fd1df13ad087bffd293665296b2117
NostalgiaX-coder <iam01sweetz@gmail.com>
2026-01-06 13:23:02 +0700

    lab11_1 suc

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,23 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "Press Enter 3 times to reveal your future.\n";
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int x = rand() % 9;
+
+    cout << endl;
+    cout << "You will get " << grade[x] << " in this 261102.\n";
+
+    return 0;
+}
